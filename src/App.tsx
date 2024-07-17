f336c839501a4f2aea9529f11ee3c4c19f7f72e1
Ihor P. <ihor.pyvovarov@gmail.com>
2024-07-17 10:02:00 +0300

    add css themes and react context to toggle themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import { Suspense } from "react"
 import { Routes, Route, Link } from "react-router-dom"
+
 import { MainPageAsync } from "./pages/MainPage/MainPage.async"
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async"
+import { useTheme } from "./theme/useTheme"
 
-import './index.scss'
+import './styles/index.scss'
 
 export function App() {
+  const {theme, toggleTheme} = useTheme();
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+
+        <button onClick={toggleTheme}>Toggle theme</button>
+
         <Link to={'/'}>Main page</Link>
         <Link to={'/about'}>About page</Link>
         <Suspense fallback={<div>Loading...</div>}>
---
+++ src/theme/ThemeProvider.tsx
@@ -0,0 +1,19 @@
+import { useState, useMemo, FC } from "react"
+import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from "./ThemeContext"
+
+const defaultTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme || Theme.DARK
+
+export const ThemeProvider: FC = ({children}) => {
+    const [theme, setTheme] = useState<Theme>(defaultTheme)
+
+    const defaultProps = useMemo(() => ({
+        theme: theme,
+        setTheme: setTheme
+    }), [theme])
+
+    return (
+        <ThemeContext.Provider value={defaultProps}>
+            {children}
+        </ThemeContext.Provider>
+    )
+}
